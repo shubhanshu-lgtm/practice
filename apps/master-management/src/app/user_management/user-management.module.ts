bd1bfcf52e771f00d1d737a5196b23bab6da23bc
Shubhanshu Kumar <shubhanshu@intercert.com>
2026-02-11 12:46:21 +0530

    fixed error

--- apps/master-management/src/app/user_management/user-management.module.ts
+++ apps/master-management/src/app/user_management/user-management.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User, Department, Team, LoginSession } from '../../../../../libs/database/src';
+import { User, Department, Team, LoginSession, DBModule } from '../../../../../libs/database/src';
 import { SystemModule } from '../../../../../libs/database/src/entities/systemModule.entity';
 import { UserManagementController } from './user-management.controller';
 import { UserManagementService } from './user-management.service';
 import { UserRepository } from '../../../../../libs/database/src/repositories/user.repository';
+import { ConfigModule } from '../../../../../libs/config/config.module';
+import { S3Module } from '../../../../../libs/S3-Service/s3.module';
+import { ResponseHandlerModule } from '../../../../../libs/response-handler/response-handler.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, SystemModule, Department, Team, LoginSession])],
+  imports: [
+    DBModule.forRoot(),
+          ConfigModule, 
+          S3Module,
+          ResponseHandlerModule,TypeOrmModule.forFeature([User, SystemModule, Department, Team, LoginSession])],
   controllers: [UserManagementController],
   providers: [UserManagementService, UserRepository],
 })
